6970f3a2dd32cad1c65e095a67c2aeb8c7fd8f20
Victor Eleanya <victoreleanya89@gmail.com>
2022-07-13 13:44:18 +0100

    added loading and error message to improve user experience

--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -1,11 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import Header from "./Header";
 import useAutocomplete from "./useAutocomplete";
 import { useRouter } from "next/router";
-import { useContext } from "react";
 import { ThemeContext } from "./Layout";
 
 const Countries = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
   const { lightTheme } = useContext(ThemeContext);
   const [countries, setCountries] = useState<any[]>();
   const [input, setInput] = useState<string>("");
@@ -18,14 +19,22 @@ const Countries = () => {
     if (!input) {
       const fetchData = async () => {
         const res = await fetch("https://restcountries.com/v2/all");
+        if (!res.ok) return setError(true);
+        setLoading(true);
         const data = await res.json();
+        setLoading(false);
+        setError(false);
         setCountries(data);
       };
       fetchData();
     } else {
       const getCountry = async () => {
         const res = await fetch(`https://restcountries.com/v2/name/${query}`);
+        if (!res.ok) return setError(true);
+        setLoading(true);
         const data = await res.json();
+        setLoading(false);
+        setError(false);
         setCountries(data);
       };
       getCountry();
@@ -66,64 +75,73 @@ const Countries = () => {
         onSelect={handleSelect}
       />
       <div className="px-12 space-y-10 md:grid sm:grid-cols-2 sm:gap-10 sm:space-y-0 lg:grid-cols-4">
-        {filtered?.map((country, index) => (
-          <div
-            key={index}
-            onClick={() =>
-              router.push({
-                pathname: "/Countries/[countrytld]",
-                query: { countrytld: country.alpha2Code },
-              })
-            }
-            className={` cursor-pointer ${
-              !lightTheme ? "bg-[#2b3945]" : "bg-white"
-            } rounded-md`}
-          >
-            <img
-              className="h-[12rem] w-[100%] rounded-t-md"
-              src={country.flags.png}
-            />
-            <div className="py-6 space-y-4 px-6">
-              <h2
-                className={`${
-                  !lightTheme ? "text-white" : "text-[#111517]"
-                } font-extrabold`}
-              >
-                {country.name}
-              </h2>
-              <div className="pb-3">
-                <h3
+        {loading && <p className="text-slate-300">Loading...</p>}
+        {error ? (
+          <p className="text-red-700 md:text-lg">
+            Oops!! Country does not exist, please double check
+          </p>
+        ) : (
+          filtered?.map((country, index) => (
+            <div
+              key={index}
+              onClick={() =>
+                router.push({
+                  pathname: "/countries/[countrytld]",
+                  query: { countrytld: country.alpha2Code },
+                })
+              }
+              className={` cursor-pointer ${
+                !lightTheme ? "bg-[#2b3945]" : "bg-white"
+              } rounded-md`}
+            >
+              <img
+                className="h-[12rem] w-[100%] rounded-t-md"
+                src={country.flags.png}
+              />
+              <div className="py-6 space-y-4 px-6">
+                <h2
                   className={`${
                     !lightTheme ? "text-white" : "text-[#111517]"
-                  } font-semibold`}
+                  } font-extrabold`}
                 >
-                  Population:{" "}
-                  <span className="font-thin opacity-70">
-                    {country.population}
-                  </span>
-                </h3>
-                <h3
-                  className={`${
-                    !lightTheme ? "text-white" : "text-[#111517]"
-                  } font-semibold`}
-                >
-                  Region:{" "}
-                  <span className="font-thin opacity-70">{country.region}</span>
-                </h3>
-                <h3
-                  className={`${
-                    !lightTheme ? "text-white" : "text-[#111517]"
-                  } font-semibold`}
-                >
-                  Capital:{" "}
-                  <span className="font-thin opacity-70">
-                    {country.capital}
-                  </span>
-                </h3>
+                  {country.name}
+                </h2>
+                <div className="pb-3">
+                  <h3
+                    className={`${
+                      !lightTheme ? "text-white" : "text-[#111517]"
+                    } font-semibold`}
+                  >
+                    Population:{" "}
+                    <span className="font-thin opacity-70">
+                      {country.population}
+                    </span>
+                  </h3>
+                  <h3
+                    className={`${
+                      !lightTheme ? "text-white" : "text-[#111517]"
+                    } font-semibold`}
+                  >
+                    Region:{" "}
+                    <span className="font-thin opacity-70">
+                      {country.region}
+                    </span>
+                  </h3>
+                  <h3
+                    className={`${
+                      !lightTheme ? "text-white" : "text-[#111517]"
+                    } font-semibold`}
+                  >
+                    Capital:{" "}
+                    <span className="font-thin opacity-70">
+                      {country.capital}
+                    </span>
+                  </h3>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </>
   );
